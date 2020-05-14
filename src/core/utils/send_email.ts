1daf191c3e9bc0198e2ecb29db0005aa4cbf28f9
deverpham <deverpham@gmail.com>
2020-05-14 17:42:01 +0700

    send chat notification

---
+++ src/core/utils/send_email.ts
@@ -0,0 +1,48 @@
+import bluebird from "bluebird";
+import ejs from "ejs";
+import fs from "fs";
+import sendInstance from "gmail-send";
+import inlineCss from "inline-css";
+import path from "path";
+const ejsPromise = bluebird.promisifyAll(ejs);
+export const sendMail = async (opts: {
+  to: string;
+  subject: string;
+  html: string;
+}) => {
+  const send = sendInstance({
+    user: "devergroup.io@gmail.com",
+    pass: "Thinh1212@@",
+    to: opts.to,
+    subject: opts.subject
+  });
+  return send({
+    html: opts.html
+  });
+};
+
+export const sendChatNotification = async (opts: {
+  to: string;
+  notification: {
+    msg: string;
+    link: string;
+  };
+}) => {
+  const html = await ejsPromise.renderFile(
+    path.join(__dirname, "./templates/chat-notifcation.template.ejs"),
+    opts.notification
+  );
+  const finalHtml = await inlineCss(html, {
+    extraCss: fs
+      .readFileSync(
+        path.join(__dirname, "./templates/chat-notifcation.template.css")
+      )
+      .toString(),
+    url: "https://dashboard.devergroup.io"
+  });
+  return sendMail({
+    to: opts.to,
+    html: finalHtml,
+    subject: "Customer is messaging to yu"
+  });
+};
